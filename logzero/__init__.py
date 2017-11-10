e3dbfa416a712725bf5e0a48b0dd386e21c03581
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-11-10 07:33:52 +0100

    changed setting of logger object loglevel
    If setting a new loglevel the loglevel of the default logger
    StreamHandler is changed to the specified. The loglevel of the
    logger object is only changed if the specified loglevel is lower
    then the previous loglevel.

--- logzero/__init__.py
+++ logzero/__init__.py
@@ -342,7 +342,10 @@ def loglevel(level=logging.DEBUG, update_custom_handlers=False):
     :arg int level: Minimum `logging-level <https://docs.python.org/2/library/logging.html#logging-levels>`_ to display (default: `logging.DEBUG`).
     :arg bool update_custom_handlers: If you added custom handlers to this logger and want this to update them too, you need to set `update_custom_handlers` to `True`
     """
-    logger.setLevel(level)
+    global _loglevel
+
+    if level < _loglevel:
+        logger.setLevel(level)
 
     # Reconfigure existing internal handlers
     for handler in list(logger.handlers):
@@ -354,7 +357,6 @@ def loglevel(level=logging.DEBUG, update_custom_handlers=False):
             # Update the loglevel for all default handlers
             handler.setLevel(level)
 
-    global _loglevel
     _loglevel = level
 
 
